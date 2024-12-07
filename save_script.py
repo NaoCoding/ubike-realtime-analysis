0e01b35f359e165ddf98aed8dddf80754e9c56eb
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-12-07 14:27:34 +0800

    feat : improve storage saving of save_script.py

--- save_script.py
+++ save_script.py
@@ -1,10 +1,25 @@
 import requests
+import json
 import datetime
 import time
 
-file = requests.get("https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json")
+
+url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+
 
 while 1:
-    with open('./data/' + datetime.datetime.now().strftime("%Y%m%d%H%M") + '.json', 'w' , encoding="utf_8_sig") as f:
-        f.write(file.text)
-    time.sleep(60)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        filtered_data = []
+        for station in data:
+            filtered_station = station["available_rent_bikes"]
+            filtered_data.append(filtered_station)
+
+        with open("./data/" +datetime.datetime.now().strftime("%Y%m%d%H%M")+".json", "a") as file:
+            file.write(json.dumps(filtered_data) + "\n")
+
+    time.sleep(180)
