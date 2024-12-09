609db8f50145204479614acac609e630f83f6bf3
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2024-12-09 15:48:44 +0800

    feat : slide fetch data from api

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 async function getSlideRange() {
     try {
-        const response = await fetch('./slide-range');
+        const response = await fetch('./api/get_time');
         if (!response.ok) {
             throw new Error('Response Error');
         }
@@ -10,3 +10,17 @@ async function getSlideRange() {
        console.error('Error fetching slide range:', error);
    }
 }
+
+
+async function updateSlideRange() {
+    const slideRange = await getSlideRange();
+    const start = slideRange.start;
+    const end = slideRange.end;
+    const slider = document.getElementById('slide');
+    slider.setAttribute('min', Number(start));
+    slider.setAttribute('max', Number(end));
+    slider.setAttribute('value', Number(start));
+    slider.setAttribute('step', 1);
+}
+
+window.onload = updateSlideRange()
